347b1aaf71b99aeeb886cceff0148c3fb8910aeb
Mr.Igor <iv_mirsoft@ukr.net>
2025-07-15 13:21:13 +0300

    08

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,8 +2,8 @@ function getFirstElement<T>(arr: T[]): T {
 	return arr[0]
 }
 
-const a = getFirstElement<number>([1, 2, 3]) // 1
-const b = getFirstElement<string>(["a", "b", "c"]) // "a"
-const c = getFirstElement<boolean>([true, false, true]) // true
+getFirstElement<number>([1, 2, 3]) // 1
+getFirstElement<string>(["a", "b", "c"]) // "a"
+getFirstElement<boolean>([true, false, true]) // true
 
 document.body.insertAdjacentHTML("beforeend", `<h2>task - 06</h2>`)
